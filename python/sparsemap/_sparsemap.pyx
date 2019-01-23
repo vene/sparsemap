36f8d19a73576377e59facbe0a7386bbd7617a68
Vlad Niculae <vlad@vene.ro>
2019-01-23 10:38:54 +0000

    more streamlined pipeline without memory for backward pass

--- python/sparsemap/_sparsemap.pyx
+++ python/sparsemap/_sparsemap.pyx
@@ -93,3 +93,57 @@ cpdef sparsemap(PGenericFactor f,
     }
 
     return post_unaries_np, post_additionals_np, solver_data
+
+
+cpdef sparsemap_fwd(PGenericFactor f,
+                    vector[double] unaries,
+                    vector[double] additionals,
+                    int max_iter=10,
+                    int verbose=0):
+
+    cdef:
+        int i, n_active, n_var, n_add
+
+        vector[BinaryVariable*] variables
+
+        vector[double] post_unaries
+        vector[double] post_additionals
+        vector[Configuration] active_set_c
+        vector[double] distribution
+
+        GenericFactor* gf
+
+    n_var = unaries.size()
+
+    cdef FactorGraph fg
+    fg.SetVerbosity(verbose)
+
+    variables.resize(n_var)
+    for i in range(n_var):
+        variables[i] = fg.CreateBinaryVariable();
+
+    fg.DeclareFactor(<Factor*> f.thisptr, variables, False)
+
+    gf = <GenericFactor*?> f.thisptr
+    gf.SetQPMaxIter(max_iter)
+    gf.SetClearCache(False)  # because we need the cache
+    f.thisptr.SetAdditionalLogPotentials(additionals)
+    f.thisptr.SolveQP(unaries, additionals, &post_unaries, &post_additionals)
+
+    active_set_c = gf.GetQPActiveSet()
+    distribution = gf.GetQPDistribution()
+
+    n_active = active_set_c.size()
+    n_add = post_additionals.size()
+
+    post_unaries_np = asfloatvec(post_unaries.data(), n_var)
+    post_additionals_np = asfloatvec(post_additionals.data(), n_add)
+    distribution_np = asfloatvec(distribution.data(), n_active)
+    active_set_py = [f._cast_configuration(x) for x in active_set_c]
+
+    solver_data = {
+        'active_set': active_set_py,
+        'distribution': distribution_np,
+    }
+
+    return post_unaries_np, post_additionals_np, solver_data
